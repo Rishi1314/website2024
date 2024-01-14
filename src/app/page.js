5d58dc6c12c3b4fcd8402a0c36525f902e411855
Rishi1314 <rishirajprof@gmail.com>
2024-01-15 00:36:02 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,267 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import {
+  FaRegEye,
+  FaRegMoon,
+  FaRegSun,
+  FaGithub,
+  FaLinkedinIn,
+  FaXTwitter,
+} from "react-icons/fa6";
+import toshare from "./images/toshare.JPG";
+import appleclonse from "./images/appleclone.JPG";
 
 export default function Home() {
+  const [theme, settheme] = useState("dark");
+  const projects = [
+    {
+      projectTile: "ToShare",
+      techstack: ["ReactJS", "Firebase"],
+      link: "https://toshare.vercel.app/",
+      source: "bg-[url('./images/toshare.JPG')]",
+    },
+    {
+      projectTile: "ToShare",
+      techstack: ["ReactJS", "Firebase"],
+      link: "https://toshare.vercel.app/",
+      source: "bg-[url('./images/toshare.JPG')]",
+    },
+    {
+      projectTile: "ToShare",
+      techstack: ["ReactJS", "Firebase"],
+      link: "https://toshare.vercel.app/",
+      source: "bg-[url('./images/toshare.JPG')]",
+    },
+    {
+      projectTile: "Apple Clone",
+      techstack: ["NextJS", "Framer-Motion"],
+      link: "https://appleclone-gilt.vercel.app/",
+      source: "bg-[url('./images/appleclone.JPG')]",
+    },
+  ];
+  const techStack = [
+    "Javascript",
+    "ReactJS",
+    "NextJS",
+    "MongoDB",
+    "NodeJS",
+    "ExpressJS",
+    "Firebase",
+    "TailwindCSS",
+    "HTML",
+    "CSS",
+    "MySql",
+  ];
+  // `${theme === "dark" ? "bg-black" : "bg-white"}`}
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={`overflow-hidden`}>
+      <div
+        className={`
+        ${theme === "dark" ? "bg-black" : "bg-white"}
+        w-full
+        min-h-screen relative overflow-hidden
+        max-[767px]:h-[100vh]
+        `}
+      >
+        {/* <div className={`flex ${theme==="dark"?"flex-row":"flex-row-reverse"} fixed top-1 right-1`}>
+          <button className="text-2xl	rounded-full bg-orange-500	text-white	 p-1 m-1" onClick={()=>{settheme("light")}}>
+            <FaRegSun />
+          </button>
+          <button className="text-2xl	rounded-full bg-cyan-900 text-white	 p-1 m-1	" onClick={()=>{settheme("dark")}}>
+            <FaRegMoon />
+          </button>
+        </div> */}
+        <motion.div
+          initial={{ y: 100 }}
+          transition={{
+            duration: 1,
+            // delay: 3,
+          }}
+          animate={{ y: 0 }}
+          className="
+          max-[767px]:h-[350px] 
+          z-10 absolute  bottom-0 left-0 w-full h-[500px] bg-[url('./images/heroman.png')] 
+          bg-contain	bg-bottom	bg-no-repeat"
+        ></motion.div>
+        <div className="relative flex flex-col w-full h-screen">
+          <motion.span
+            initial={{ x: "50%" }}
+            transition={{
+              duration: 1,
+              // delay: 3,
+            }}
+            animate={{ x: "0%" }}
+            className=" 
+            max-[767px]:text-[18vw] 
+            max-[767px]:top-[30%]
+            min-[768px]:text-[200px] absolute  
+            min-[768px]:top-0 min-[768px]:right-0 text-transparent stroke-white	outlined 
+            font-mukta">
+            HEY, THERE!
+          </motion.span>
+          <motion.span
+            initial={{ y: "-100%" }}
+            transition={{
+              duration: 1,
+              // delay: 3,
+            }}
+            animate={{ y: "0%" }}
+            className="
+            max-[767px]:top-[41%]
+            max-[767px]:text-[15vw] 
+            min-[768px]:top-[20%] min-[768px]:left-10 
+            min-[768px]:text-[100px] absolute
+             bg-white text-black 
+            font-lexend "
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            I AM
+          </motion.span>
+          <motion.span
+            initial={{ x: "-50%" }}
+            transition={{
+              duration: 1,
+              // delay: 3,
+            }}
+            animate={{ x: "0%" }}
+            className="
+            max-[767px]:top-[51%]
+            max-[767px]:text-[15vw] 
+            min-[768px]:text-[150px] absolute 
+            min-[768px]:top-[50%] min-[768px]:left-0 text-white 
+            font-lexend  w-full"
+          >
+            DEVELOPER
+          </motion.span>
+          
+          <motion.span
+            initial={{ x: "50%" }}
+            transition={{
+              duration: 1,
+              // delay: 3,
+            }}
+            animate={{ x: "0%" }}
+            className=" 
+            max-[767px]:top-[61%]
+            max-[767px]:text-[7vw] 
+            min-[768px]:text-[120px] 
+            min-[768px]:right-0 min-[768px]:top-[70%] 
+            absolute 
+             
+            text-white font-lexend "
+          >
+            BUILDER
+          </motion.span>
+        </div>
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={{ x: "0%" }}
+          className={`
+          max-[767px]:top-0 max-[767px]:left-0 min-[768px]:top-1/3 min-[768px]:left-0
+          
+          max-[767px]:py-3 max-[767px]:px-5 max-[767px]:border-t-0 min-[768px]:flex-col min-[768px]:border-l-0
+          
+           ${
+             theme === "dark"
+               ? "text-white bg-black border-white"
+               : "text-black border-black"
+           } flex justify-center items-center	  z-10 fixed gap-4 border-2 py-6 px-3 rounded-r-lg`}
+        >
+          <a
+            className="text-2xl hover:scale-150 hover:text-orange-800 duration-150"
+            href="https://github.com/Rishi1314"
+            target="blank"
+          >
+            <FaGithub />
           </a>
+          <a
+            className="text-2xl hover:scale-150 hover:text-sky-400 duration-150"
+            href="https://www.linkedin.com/in/rishi-raj-a36930206/"
+            target="blank"
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            className="text-2xl hover:scale-150	duration-150"
+            href="https://twitter.com/rishirajbdev"
+            target="blank"
+          >
+            <FaXTwitter />
+          </a>
+        </motion.div>
+      </div>
+      <div className="items-center justify-center flex flex-col text-white bg-neutral-900	">
+        <div className="flex w-[100%] flex-col justify-center items-center">
+          <h1 className="my-6 max-[767px]:text-2xl min-[768px]:text-4xl">
+            My Projects
+          </h1>
+          <div className="relative flex flex-wrap gap-6 max-[767px]:w-[90%] min-[768px]:w-1/2">
+            <a href="https://appleclone-gilt.vercel.app" target="blank">
+            <div className="shadow-[0_8px_30px_rgb(0,0,0,0.12)] text-center flex flex-col relative overflow-hidden rounded-[20px] bg-black pt-20
+             min-[768px]:w-[700px] min-[768px]:h-[400px]
+             max-[767px]:w-[100%]
+            ">
+              <motion.video autoPlay muted className="z-0">
+                <source src="/iphoneherovideo.mp4" type="video/mp4" />
+              </motion.video>
+              <div className="flex flex-col z-1">
+                <span className="text-2xl w-full max-[767px]:text-[5vw] absolute top-0  font-lexend text-[#3a75f5]">
+                  {"Iphone "}
+                  <span className=" text-[#5689f8]">Website</span>
+                </span>
+                <span className="w-full max-[767px]:text-[3vw] absolute top-6 ">
+                  Built using NextJS and Framer-Motion
+                </span>
+              </div>
+            </div>
+            </a>
+            <a href="https://toshare.vercel.app" target="blank">
+            <div className="shadow-[0_8px_30px_rgb(0,0,0,0.12)] 
+             min-[768px]:w-[700px] min-[768px]:min-h-[400px]
+             max-[767px]:min-w-[90vw] max-[767px]:h-[30vh]
+            
+            justify-center items-center flex flex-col relative overflow-hidden rounded-[20px] bg-[#FAFAFA] pt-20">
+              
+              <motion.div 
+            transition={{
+              duration: 1,
+              // delay: 4,
+            }}
+            initial={{
+              x:"100%"
+            }}
+            whileInView={{
+              x:"0%"
+            }} viewport={{ once: true }} 
+            className="absolute bottom-0 bg-[url('./images/toshare.JPG')] 
+            bg-contain bg-bottom bg-no-repeat 
+            rounded-[20px] 
+            w-[100%]
+            min-h-[100%]
+          "></motion.div>
+            <div className="absolute top-0 flex flex-col text-center text-black z-2 mb-1">
+                <span className="text-4xl max-[767px]:text-[5vw] font-lexend text-[#69ADFA] leading-none">ToShare</span>
+                <span className="max-[767px]:text-[3vw] font-mukta">Decentralized File Sharing.</span>
+              </div>
+              {/* <motion.div
+              transition={{
+                duration: 1,
+                // delay: 4,
+              }}
+              initial={{
+                y:400
+              }}
+              whileInView={{
+                y:100
+              }} viewport={{ once: true }}
+              className="absolute bottom-0 left-0 bg-[url('./images/tosharemobile.png')] bg-contain bg-center bg-no-repeat w-[200px] h-[400px]"></motion.div> */}
+            </div>
+            </a>
+
+            
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,32 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'bandw': "url(/bandw.png')",
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+
+      homemadeApple: ['Homemade Apple', 'cursive'],
+      mukta: ['Mukta', 'sans-serif'],
+      lexend: ['Lexend', 'sans-serif'],
+    },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
     },
   },
   plugins: [],
